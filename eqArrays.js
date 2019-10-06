ce23985694096d7143ae34e89cf9cfe500acbe78
Jonathan Liu <jxliu95@gmail.com>
2019-10-06 22:02:31 +0000

    Fix eqArrays.js (recursion)

--- eqArrays.js
+++ eqArrays.js
@@ -2,7 +2,9 @@ const eqArrays = (sinister, dexter) => {
   if (sinister.length !== dexter.length) return false;
 
   for (let i = 0; i < dexter.length; i++) {
-    if (sinister[i] !== dexter[i]) return false;
+    if (Array.isArray(dexter[i])) {
+      if (!eqArrays(sinister[i], dexter[i])) return false;
+    } else if (sinister[i] !== dexter[i]) return false;
   }
 
   return true;
