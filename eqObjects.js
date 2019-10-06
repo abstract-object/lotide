ca5195d5ad2c34295cbb6ce6273d072e5962eb95
Jonathan Liu <jxliu95@gmail.com>
2019-10-06 21:58:33 +0000

    Fix eqObjects.js (recursion)

--- eqObjects.js
+++ eqObjects.js
@@ -6,6 +6,8 @@ const eqObjects = (obj1, obj2) => {
   for (let key of keys1) {
     if (Array.isArray(obj1[key])) {
       if (!eqArrays(obj1[key], obj2[key])) return false;
+    } else if (typeof obj1[key] === "object") {
+      if (!eqObjects(obj1[key], obj2[key])) return false;
     } else if (obj1[key] !== obj2[key]) return false;
   }
   return true;
