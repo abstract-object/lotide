bc13751f21728d63cf329b44ed5f77826dad1396
Jonathan Liu <jxliu95@gmail.com>
2019-10-06 21:28:24 +0000

    Add eqObjects.js (incomplete)

---
+++ eqObjects.js
@@ -0,0 +1,14 @@
+const eqArrays = require("./eqArrays");
+
+const eqObjects = (obj1, obj2) => {
+  const keys1 = Object.keys(obj1);
+  if (keys1.length !== Object.keys(obj2).length) return false;
+  for (let key of keys1) {
+    if (Array.isArray(obj1[key])) {
+      if (!eqArrays(obj1[key], obj2[key])) return false;
+    } else if (obj1[key] !== obj2[key]) return false;
+  }
+  return true;
+};
+
+module.exports = eqObjects;
